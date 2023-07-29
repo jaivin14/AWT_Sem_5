8f5b3e6ddf86e6864373cb105453faba620af73f
JaivinSavaliya <jpsavaliya14@gmail.com>
2023-07-29 15:11:45 +0530

    Practical 1 to 4

---
+++ Practical list/P1.js
@@ -0,0 +1,15 @@
+const StringName = "Jaivin"
+const Roll = 120
+const Admission = true
+
+console.log(StringName);
+console.log(Roll);
+console.log(Admission);
+
+
+console.log("Running the function");
+const fun = (a,b) => {
+    return a + b
+}
+
+console.log(fun(2,3));
